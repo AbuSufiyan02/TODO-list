46ca653cec5124c623947705f7f0e9fc05660442
AbuSufiyan02 <165479744+AbuSufiyan02@users.noreply.github.com>
2024-04-14 08:43:53 +0500

    Created a TODO-List by using inquirer & chalk

---
+++ index.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+console.log(chalk.blueBright.bold("\n\tWelcome to my TODO-List Application\n"));
+
+let todolist = [];
+let conditions = true;
+
+while(conditions){
+    let addtask = await inquirer.prompt(
+        {
+            name: "task",
+            message:"\n\tEnter a new Task:",
+            type: "input"
+        }
+    )
+    todolist.push(addtask.task);
+    console.log(`\n\t${addtask.task} is added to your list`);
+
+    let addmore = await inquirer.prompt(
+        {
+            name: "moretask",
+            message: "\n\tWant to add more task? ",
+            type: "confirm",
+            default: "false"
+        }
+    )
+    conditions = addmore.moretask;
+    
+}
+console.log("\n\tYour Updated TODO-List is: " ,todolist);
